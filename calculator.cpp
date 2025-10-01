fb7a2a1226679b316f9f10921bbaa379b7732732
Korsun Bohdan <korsun.bohdan@lll.kpi.ua>
2025-10-01 23:55:43 +0300

    Older patches for recovery

--- calculator.cpp
+++ calculator.cpp
@@ -14,3 +14,16 @@ int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+
+// new function — Factorial
+long Calculator::Factorial(int a)
+{
+    if (a < 0)
+        throw std::invalid_argument("Factorial is undefined for negative numbers.");
+
+    long result = 1;
+    for (int i = 2; i <= a; ++i)
+        result *= i;
+
+    return result;
+}
--- calculator.h
+++ calculator.h
@@ -7,6 +7,8 @@ class Calculator
         int Add (double, double);
         int Sub (double, double);
         int Mul (double, double);
+	// my function:
+        long Factorial(int); 
 };
 
 #endif//CALCULATOR_H
